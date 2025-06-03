97a5a38667968c48021f1256c98db24142fb76ca
RayeneBACHA <bacharayene7@gmail.com>
2025-06-03 16:30:41 +0000

    added main.c

---
+++ main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+
+
+int main(int argc, char const *argv[])
+{
+    printf("Hellow world!\n");
+    printf("Hellow world!\n");
+    printf("Hellow world!\n");
+
+    return 0;
+}
